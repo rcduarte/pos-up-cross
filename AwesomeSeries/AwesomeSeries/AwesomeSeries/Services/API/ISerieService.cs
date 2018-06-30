7154dbe83c8ab3a222f7e66963acd3dbdbc8c99e
Rafael Cavalcanti Duarte <cavalcanti_duarte@hotmail.com>
2018-06-30 13:38:50 -0300

    ...

---
+++ AwesomeSeries/AwesomeSeries/AwesomeSeries/Services/API/ISerieService.cs
@@ -0,0 +1,13 @@
+﻿using AwesomeSeries.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeSeries.Services.API
+{
+    public interface ISerieService
+    {
+        Task<IEnumerable<SerieResponse>> getSerieAsync();
+    }
+}
---
+++ AwesomeSeries/AwesomeSeries/AwesomeSeries/Services/API/SerieServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AwesomeSeries.Models;
+
+namespace AwesomeSeries.Services.API
+{
+    public class SerieService : ISerieService
+    {
+        public Task<IEnumerable<SerieResponse>> getSerieAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
